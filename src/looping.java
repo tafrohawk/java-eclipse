863bdf9805d2b8548d402eb7e6f12146b0a59f7b
Tomas Frohawk <tom@email.com>
2017-12-17 17:17:58 +0000

    First commit

---
+++ src/looping.java
@@ -0,0 +1,24 @@
+public class looping {
+	public static void main(String args[]) {
+		
+		System.out.println("Hello World!");
+		boolean check = true;
+		int count = 0;
+		// do while looping
+		do {
+			
+			// check if 'count' variable is more than 100, if it is, set 'check' to false.
+			if (count > 100) {
+				// set 'check' to false.
+				check = false;
+			}
+			// print 'count' variable value to console.
+			System.out.println(count);
+			
+			// increment 'count' variable by 1.
+			count++;
+			
+		} while (check == true);
+		
+	}
+}
